954ff97962489e6d245f7b3319cce6c12ab86bce
daltonoswald <daltonoswald@gmail.com>
2024-04-16 14:15:42 -0400

    each guess mapped out

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import { useState, useEffect } from 'react'
 import {format } from 'date-fns'
 import { bossList } from './bossList'
@@ -5,7 +6,7 @@ import reactIcon from "./assets/react.svg"
 import SearchableDropdown from './SearchableDropdown';
 
 function App() {
-  const [guessedBoss, setGuessedBoss] = useState('');
+  const [guessedBosses, setGuessedBosses] = useState([]);
   const [guessedIndex, setGuessedIndex] = useState(-1);
   const [correctBoss, setCorrectBoss] = useState({});
   const [status, setStatus] = useState('Please select a guess');
@@ -29,15 +30,15 @@ function App() {
   },[])
 
   console.log(`The correctBoss name  ${correctBoss.name}`);
-  console.log(`You have guessed ${name}`)
-  console.log(`You have guessed ${guessedBoss}`)
+  console.log(`You have guessed ${guessedBosses}`)
+  console.log(guessedBosses)
 
   function handleGuess(name) {
     const guessedNameValue = name;
     console.log(guessedNameValue);
     let indexVal = bossList.findIndex(x => x.name === `${guessedNameValue}`)
-    setGuessedBoss(guessedNameValue);
-    setGuessedIndex(indexVal);
+    setGuessedBosses([...guessedBosses, indexVal]);
+    console.log(guessedBosses);
     console.log(guessedNameValue)
     console.log(indexVal);
     if (correctBoss.name === guessedNameValue) {
@@ -49,19 +50,6 @@ function App() {
     }
   }
 
-  function showAll(e) {
-    let bossVal = e.target.value;
-    setGuessedIndex(e.target.value);
-    console.log(bossVal);
-    console.log(bossList[bossVal].name)
-    console.log(bossList[bossVal].level)
-    console.log(bossList[bossVal].health)
-    console.log(bossList[bossVal].attack)
-    console.log(bossList[bossVal].region)
-    console.log(bossList[bossVal].release)
-    console.log(bossList[bossVal].hasPet)
-  }
-
   // let keys = Object.keys(bossList);
   // for (let i = 0; i < keys.length; i++) {
   //   let val = bossList[keys[i]];
@@ -70,23 +58,32 @@ function App() {
 
   // console.log(bossList[28])
 
-
+  if (guessedBosses.length === 0) {
+    return (
+      <>
+      <div>No guesses yet</div>
+      <div>The correct boss was {correctBoss.name} {correctBoss.level} {correctBoss.health} {correctBoss.attack} {correctBoss.region} {correctBoss.release} {correctBoss.hasPet} </div>
+      <div>{status}</div>
+  
+      <BossesGuessed guessedIndex={guessedIndex} guessedBosses={guessedBosses} />
+  
+      <SearchableDropdown 
+        options={bossList}
+        label="name"
+        id="id"
+        selectedVal={value}
+        handleChange={(val) => setValue(val)}
+        handleGuess={handleGuess}
+        />
+  
+      </>
+    )
+  } else {
   return (
     <>
-    <div>You have guessed {guessedBoss}</div>
+    {/* <div>You have guessed {bossList[guessedBosses].name}</div> */}
     <div>The correct boss was {correctBoss.name} {correctBoss.level} {correctBoss.health} {correctBoss.attack} {correctBoss.region} {correctBoss.release} {correctBoss.hasPet} </div>
     <div>{status}</div>
-
-    <button id="25" onClick={showAll}>25</button>
-    <input htmlFor="bossId" type='number' onChange={showAll}></input>
-    <select 
-      value={guessedBoss}
-      onChange={handleGuess}
-    >
-      {bossList.map((boss) => <option key={boss.name} id={boss.name} value={boss.name}>{boss.name}</option>)}
-    </select>
-
-    <GuessedIndexInfo guessedIndex={guessedIndex} bossList={bossList} />
 
     <SearchableDropdown 
       options={bossList}
@@ -97,31 +94,37 @@ function App() {
       handleGuess={handleGuess}
       />
 
+    <BossesGuessed guessedBosses={guessedBosses} bossList={bossList} />
+
     </>
   )
 }
+}
 
-function GuessedIndexInfo({guessedIndex, bossList}) {
-  console.log(bossList[guessedIndex])
+function BossesGuessed({guessedBosses, bossList}) {
 
-  if (guessedIndex === -1) {
+  if (guessedBosses.length === 0) {
     return (
-      <div>make a guess</div>
+      <div></div>
   )
   } else {
     return (
-      <div>
-        <div>{(bossList[guessedIndex].name)}</div>
-        <div>{(bossList[guessedIndex].level)}</div>
-        <div>{(bossList[guessedIndex].health)}</div>
-        <div>{(bossList[guessedIndex].attack)}</div>
-        <div>{(bossList[guessedIndex].region)}</div>
-        <div>{(bossList[guessedIndex].release)}</div>
-        <div>{(bossList[guessedIndex].hasPet)}</div>
+      <div className="guess-container">
+        {guessedBosses.map((boss) => 
+          <div className="guess">
+            <div>{(bossList[boss].name)}</div>
+            <div>{(bossList[boss].level)}</div>
+            <div>{(bossList[boss].health)}</div>
+            <div>{(bossList[boss].attack)}</div>
+            <div>{(bossList[boss].region)}</div>
+            <div>{(bossList[boss].release)}</div>
+            <div>{(bossList[boss].hasPet)}</div>
+          </div>
+        )}
       </div>
-    )
+      )
   }
-
 }
+
 
 export default App
