c8817fe7801dbd1f17661595cca3d2266eca1d00
daltonoswald <daltonoswald@gmail.com>
2024-11-06 12:17:26 -0500

    share button added to score panel

--- src/endPanel/EndPanel.jsx
+++ src/endPanel/EndPanel.jsx
@@ -1,9 +1,23 @@
 import "./endPanel.css";
 function EndPanel({ correctBoss, guessCount, scoreSheet, setScoreSheet }) {
+
     console.log(scoreSheet);
 
+    function shareableString(){
+        const copyMessage = `I solved the daily Runescapedle in ${guessCount} attempts!`
+                // Working with an extra space in last result -
+        // navigator.clipboard.writeText(copyMessage + "\n" + (scoreSheet.toString().replaceAll(" ", "\n").replaceAll(",", "")))
+        const copyMessageLink = `https://daltonoswald-runescapedle.netlify.app`
+        navigator.clipboard.writeText(
+                copyMessage + 
+                "\n" + 
+                scoreSheet.toString().replace(/(.*?,.*?,.*?,.*?,.*?,.*?,.*?,)/g, '$1'+'\n').replaceAll(",", "") + 
+                "\n" + 
+                copyMessageLink)
+    }
+
     return(
-        <div className={`end-panel`}>
+        <div className='end-panel'>
             <div className='win-message'>You won!</div>
             <div className="correct-answer">
                 <img className="correct-image" src={(correctBoss.image)} />
@@ -11,14 +25,13 @@ function EndPanel({ correctBoss, guessCount, scoreSheet, setScoreSheet }) {
             </div>
             <div className="number-attempts">Number of attempts: {guessCount}</div>
             {scoreSheet.map(score => (
-                <p className="scoreline">{score}</p>
+                <p key={score} className="scoreline">{score}</p>
             ))}
+            {/* <button onClick={() => {navigator.clipboard.writeText(scoreSheet.toString().replace(/[!.,]/g, ''))}}>Share Results</button> */}
+            <button className="share-button" onClick={shareableString}>Share Results</button>
+
         </div>
     )
-}
-
-function GuessedScoresLayout({score}) {
-    <p>{score}</p>
 }
 
 export default EndPanel
