6db103e98fb74356f9266d6b040e333f61cf1f64
daltonoswald <daltonoswald@gmail.com>
2025-03-27 11:27:49 -0400

    css changes on nav and help module typo fixed

--- src/help/Help.jsx
+++ src/help/Help.jsx
@@ -12,7 +12,7 @@ export default function Help({ setOpenHelp }) {
             <p>Inspired by <a href='https://www.nytimes.com/games/wordle/index.html'>Wordle</a>, each day there is a new Oldschool Runescape boss to guess.</p>
             <p>Guess a boss and then adjust your next guess based on how close your previous guesses were.</p>
             <p>Share your results and come back each day to play!</p>
-            <p>Puzzle resets are midnight.</p>
+            <p>Puzzle resets at midnight.</p>
         </div>
     )
 }
